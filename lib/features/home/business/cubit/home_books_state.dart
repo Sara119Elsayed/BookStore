fd3bcd5b5048145458d8af48692b029f58ab4fd0
SaraElsayed <esara7772@gmail.com>
2025-10-18 16:11:10 +0300

    "home_business_logic"

---
+++ lib/features/home/business/cubit/home_books_state.dart
@@ -0,0 +1,10 @@
+part of 'home_books_cubit.dart';
+
+sealed class HomeBooksState extends Equatable {
+  const HomeBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class HomeBooksInitial extends HomeBooksState {}
